61164dd99f338260bc048791b9782c901a84321d
Ashley Pereira <ashleypereira70@gmail.com>
2018-09-28 18:59:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 function forLoop(array) {
+// here we built a function with array as an argument
    for ( let i = 0; i < 25; i++) {
+// In the for loop start the count from 0(initialization), add the condition where we add a string to the array 25 times    
      if (i === 1) {
+//conditional statement, i value is equal to 1 then the below condition is met      
        array.push(`I am 1 strange loop.`);
+//if the above is not met, then print the below statement and return array        
      } else {
        array.push(`I am ${i} strange loops.`);
      }
@@ -14,13 +18,19 @@ function whileLoop(n){
   }
   return 'done';
 }
-let i = 0
+
 function doWhileLoop(array){
-  array.length > 0 && incrementVariable();
+  function incrementVariable() {
+  i = i + 1;
 }
+let i = 0;
+  
+
 do{
-  console.log(i);
-  {
-  }
-} while (i<5);
-return false
+  array = array.slice(1)
+  incrementVariable()
+  console.log(i)
+} while(array.length > 0 && i < 5);
+
+return array
+}
